0455fec5812989e77bf26c2fdb91eca9d9538852
dchamp16 <dchamp16@gmail.com>
2024-11-14 10:26:18 -0700

    Create testMongodb.js

---
+++ mongoDB/testMongodb.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const MONGODB_USERNAME = process.env.MONGODB_USERNAME;
+const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
+
+const MONGODB_URI = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@web3project.ebt9u.mongodb.net/?retryWrites=true&w=majority&appName=web3project`;
+
+console.log(MONGODB_URI);
+
+mongoose.connect(MONGODB_URI).then(() => console.log('MongoDB Connected!')).catch(err => console.log(err));
