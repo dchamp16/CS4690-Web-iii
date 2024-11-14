8596eb31013fb4f75690a8dc5be159e1ae3c3ff4
Peter Ramos <86265240+dchamp16@users.noreply.github.com>
2024-11-14 13:55:34 -0700

    update

--- mongoDB/server/seed.js
+++ mongoDB/server/seed.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
 
 const MONGODB_USERNAME = process.env.MONGODB_USERNAME;
--- mongoDB/server/server.js
+++ mongoDB/server/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 // Middleware setup
 app.use(cors({ origin: 'http://localhost:3000' }));
