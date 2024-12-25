2043b1a9137542f905b20b7a744c54479d89fd87
Peter Ramos <86265240+dchamp16@users.noreply.github.com>
2024-12-24 19:48:04 -0700

    Update vite.config.ts

--- babelfish/vite.config.ts
+++ babelfish/vite.config.ts
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: './', // This ensures the root is `babelfish`
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Output directory for production build
-    rollupOptions: {
-      input: './index.html', // Entry file for Vite
-    },
-  },
 });
